48113e1e7487c6cd8e6ea68c85a80f8e58c6a70b
ANSHUL MISHRA <2311201284@stu.manit.ac.in>
2025-03-24 06:22:46 +0000

    datatypes

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  //treat all JS code as newer version ----> but can't undo it.
+//  But in present time it is automatically incorporated with your system
+
+// alert(3+3) --> can't use it directly in Node but there exist different keywords to use it
+// In node . But in browser you can directly use it.
+
+/* Maintain Code Readability */
+
+let name="Mr. Alex"
+let age = 18
+let isLoggedIn = true
+
+// number =>  2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null (it is standalone value)
+// undefined => when we have not define any value to variable but null is representing empty variable
+// eg. suppose you want to know about tempr from server but it is not able to send tempr then it will send null.
+// symbol (majorly used in react for uniqueness)
+
+
+// object
+
+console.log(typeof null); // type of null is "object"
+console.log(typeof undefined); // type of undefined is "undefined"
+
