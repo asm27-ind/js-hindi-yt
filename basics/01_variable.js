b19897d0d1cf2c79212791e87baee0c434c83f20
ANSHUL MISHRA <2311201284@stu.manit.ac.in>
2025-03-24 06:24:24 +0000

    variables basics

--- basics/01_variable.js
+++ basics/01_variable.js
@@ -18,4 +18,5 @@ console.log(accountId)
 pref not to use var , because of issue in block scope and fnal scope.
 */
 
+
 console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
