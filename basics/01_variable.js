2b8997ce47d7341f2de695db98fd0233fc7f0c57
ANSHUL MISHRA <2311201284@stu.manit.ac.in>
2025-03-24 05:48:51 +0000

    container config done

--- basics/01_variable.js
+++ basics/01_variable.js
@@ -1,5 +1,21 @@
 const accountId=144553
 let accountEmail="asm8839@gmail.com"
 var accountPassword="12345678"
-accountCity="jaipur"
 
+accountCity="jaipur" //this way of defining is wrong but you can, by this. 
+
+let accountState; //undefined
+
+// accountId=3  // not allowed
+
+accountEmail="pqr@gmail.com"
+accountPassword="12348"
+accountCity="vijayanagar"
+
+console.log(accountId)
+
+/*
+pref not to use var , because of issue in block scope and fnal scope.
+*/
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
