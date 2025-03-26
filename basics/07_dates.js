666050c1e2a81f88bf71d2a2d8b4b32332fe2eb2
ANSHUL MISHRA <2311201284@stu.manit.ac.in>
2025-03-26 10:25:41 +0000

    date time

--- basics/07_dates.js
+++ basics/07_dates.js
@@ -7,8 +7,24 @@ let mydate=new Date()
 
 //let mynewDate = new Date(2023,0,24,5,3,30) // yr month day  hr min sec (month follow 0nbased indexing)
 //let mynewDate = new Date("2023-01-14") // yyyy-mm-dd
-let mynewDate = new Date("01-14-2023") // mm-dd-yyyy
+// let mynewDate = new Date("01-14-2023") // mm-dd-yyyy
 
 // console.log(mynewDate.toLocaleString());
 
+let mytimestamp= Date.now()
+// console.log(mytimestamp);
+// //The Date.now() function in JavaScript returns the current timestamp in milliseconds 
+// // since January 1, 1970 (UTC), also known as the Unix Epoch time.
+// console.log(mynewDate.getTime()); // return time in ms from 1 jan 1970 to 14th jan 2023.
 
+// how to convert ms to sec?
+
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newdate = new Date()
+console.log(newdate.getMonth()+1)
+console.log(newdate.getDay()) // day 1-mon 2-tue ....
+newdate.toLocaleString('default',{
+    weekday: "long"
+}) // can customize format of localstring.
