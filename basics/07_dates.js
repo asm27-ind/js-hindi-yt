b9d7c08dade326878269f7aa03c4bb8570a14d2e
ANSHUL MISHRA <2311201284@stu.manit.ac.in>
2025-03-26 09:29:18 +0000

    not completed

---
+++ basics/07_dates.js
@@ -0,0 +1,14 @@
+//Date object
+let mydate=new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleTimeString());
+
+//let mynewDate = new Date(2023,0,24,5,3,30) // yr month day  hr min sec (month follow 0nbased indexing)
+//let mynewDate = new Date("2023-01-14") // yyyy-mm-dd
+let mynewDate = new Date("01-14-2023") // mm-dd-yyyy
+
+// console.log(mynewDate.toLocaleString());
+
+
